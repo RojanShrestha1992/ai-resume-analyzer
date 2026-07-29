c6f6ec0f458165c7df0855f53efdca9cf395dd85
RojanShrestha1992 <pheonixeder@gmail.com>
2026-07-29 10:55:11 +0545

    initialize server with Express and MongoDB connection

---
+++ server/models/User.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true, "Please add a name"],
+        trim: true
+    },
+    email:{
+        type: String,
+        required: [true, "Please add an email"],
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    password:{
+        type: String,
+        required: [true, "Please add a password"],
+        minlength: [6, "Password must be at least 6 characters long"]
+    }
+},{
+    timestamps: true
+})
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
